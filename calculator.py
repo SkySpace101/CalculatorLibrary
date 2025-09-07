aa8ccd7fd31b0d305912693ea666ad5dd5f054c9
Shubham Agarwal <shubham.agarwal480@gmail.com>
2025-09-07 15:50:43 +0530

    Test-Add: Added test for multiplication function

--- calculator.py
+++ calculator.py
@@ -3,10 +3,12 @@ Calculator Library containing basic math operations
 
 """
 
-def add(first_term:float, second_term:float)->float:
+
+def add(first_term: float, second_term: float) -> float:
     """ return the Sum of the arguments"""
     return first_term + second_term
 
-def subtract(first_term:float, second_term:float)->float:
+
+def subtract(first_term: float, second_term: float) -> float:
     """ return the difference between the arguements"""
     return first_term - second_term
--- test_calculator.py
+++ test_calculator.py
@@ -17,3 +17,6 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
+
+    def test_multiplication(self):
+        assert 42 == calculator.multiply(7, 6)
