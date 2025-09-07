de91b105d326bcb68dcd44a126fcaa9f44eba152
Shubham Agarwal <shubham.agarwal480@gmail.com>
2025-09-07 14:58:54 +0530

    Add: Added Function for addition and subtraction.

---
+++ calculator.py
@@ -0,0 +1,12 @@
+"""
+Calculator Library containing basic math operations
+
+"""
+
+def add(first_term:float, second_term:float)->float:
+    """ return the Sum of the arguments"""
+    return first_term + second_term
+
+def subtract(first_term:float, second_term:float)->float:
+    """ return the difference between the arguements"""
+    return first_term - second_term
