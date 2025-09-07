40df2dacaf85b6c851d519ed063ca4bc2dc5657c
Shubham Agarwal <shubham.agarwal480@gmail.com>
2025-09-07 16:20:22 +0530

    Add: Addded multiply function to calculate.py

--- calculator.py
+++ calculator.py
@@ -12,3 +12,8 @@ def add(first_term: float, second_term: float) -> float:
 def subtract(first_term: float, second_term: float) -> float:
     """ return the difference between the arguements"""
     return first_term - second_term
+
+
+def mulitply(first_term: float, second_term: float) -> float:
+    """ return the product of the arguments"""
+    return first_term * second_term
